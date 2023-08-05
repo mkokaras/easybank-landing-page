81b0dbaf90fd38db1ff36eb26138e1efa6f00a04
mkokaras <menioskokaras1@gmail.com>
2023-08-05 15:44:23 +0300

    Finish design except hero

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./style.scss";
+
+let isOpen = false;
+
+const navBtn = document.querySelector(".nav-btn");
+
+const body = document.querySelector("body");
+
+navBtn.addEventListener("click", () => {
+  isOpen = !isOpen;
+
+  if (isOpen) {
+    body.classList.add("nav-open");
+  } else {
+    body.classList.remove("nav-open");
+  }
+});
